89004ba08896eb5a8861f0af00990f7551937ab7
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-05-21 07:06:23 -0400

    add production configuration for Heroku

--- knexfile.js
+++ knexfile.js
@@ -33,10 +33,10 @@ module.exports = {
   production: {
     client: 'mysql',
       connection: {
-        host : '127.0.0.1',
-        user : 'root',
-        password : 'root',
-        database : 'recipes_cookbook'
+        host : 'us-cdbr-east-06.cleardb.net',
+        user : 'bcd3a892affde9',
+        password : 'be75ab66',
+        database : 'heroku_43185ac3ba2fb6f'
       },
       migrations: {
         directory: './database/migrations',
